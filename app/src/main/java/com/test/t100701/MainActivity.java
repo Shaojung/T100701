d324ac03f620865c61229784546a1807e42cd878
Shaojung <shaojung@gmail.com>
2016-10-07 11:56:03 +0800

    Layout Params.

--- app/src/main/java/com/test/t100701/MainActivity.java
+++ app/src/main/java/com/test/t100701/MainActivity.java
@@ -2,6 +2,7 @@ package com.test.t100701;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -13,14 +14,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.layout2);
 
         LinearLayout l2 = (LinearLayout) findViewById(R.id.mLayout2);
+        LinearLayout sub1 = new LinearLayout(MainActivity.this);
+        sub1.setOrientation(LinearLayout.HORIZONTAL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+        );
+
+        LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        params2.weight = 1;
+
+        sub1.setLayoutParams(params);
+
         Button btn1 = new Button(MainActivity.this);
+        btn1.setLayoutParams(params2);
         btn1.setText("Button1");
-        l2.addView(btn1);
+        sub1.addView(btn1);
         Button btn2 = new Button(MainActivity.this);
+        btn2.setLayoutParams(params2);
         btn2.setText("Button2");
-        l2.addView(btn2);
+        sub1.addView(btn2);
         Button btn3 = new Button(MainActivity.this);
+        btn3.setLayoutParams(params2);
         btn3.setText("Button3");
-        l2.addView(btn3);
+        sub1.addView(btn3);
+        l2.addView(sub1);
     }
 }
